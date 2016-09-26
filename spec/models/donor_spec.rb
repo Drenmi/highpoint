3aa182346cf5d28ddf01a2a4e94dd63c9ba32904
alahab <maria.dku.kz@gmail.com>
2016-09-26 16:52:17 +0800

    Allow users to see a list of donors

--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Donor, type: :model do
   it { is_expected.to have_many(:donations).inverse_of(:donor) }
 
   it { is_expected.to validate_presence_of(:identification) }
+
+  describe "#total_donations" do
+    let(:donor) { FactoryGirl.create(:donor_with_donations) }
+
+    it "shows total amount donated" do
+      expect(donor.total_donations).to eq(200)
+    end
+  end
 end
