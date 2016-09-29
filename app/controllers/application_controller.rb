ed1078b7e738b603210231c7e2d6cfd45f996730
farrahfazirah <farrahfazirah@gmail.com>
2016-09-30 01:26:42 +0800

    Allow users to sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
+  include Clearance::Controller
   protect_from_forgery with: :exception
 
-  before_action :prepare_modal_donation
+  before_action :require_login, :prepare_modal_donation
 
   private
 
   def prepare_modal_donation
     @modal_donation = Donation.new
+  end
+
+  def index
+  	current_user.application
   end
 end
