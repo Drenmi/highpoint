a1992ebf25d10fbf17bb8309f3ac515296e33127
limxinyuuu <limxinyuuu@gmail.com>
2016-09-30 01:35:24 +0800

    Added feature to view and edit donor details on donor page

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -9,9 +9,30 @@ class DonorsController < ApplicationController
     @donors = Donor.all
   end
 
+  def update
+    @donor = find_donor(params[:id])
+
+    if @donor.update(donor_params)
+      redirect_to @donor
+    else
+      redirect_to :edit
+    end
+  end
+
+  def edit
+    @donor = find_donor(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
   private
 
   def find_donor(id)
     Donor.find(id)
+  end
+
+  def donor_params
+    params.require(:donor).permit(:name, :phone_number, :email_address, :address)
   end
 end
