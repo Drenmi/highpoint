3ec15865d86af9eb91aad3f74e11efb7f683b32f
alahab <maria.dku.kz@gmail.com>
2016-10-14 09:53:17 +0800

    Allow users to search for a donor on donors/index - Implement search - Add total donors to the view

--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Donor, type: :model do
 
     it { expect(donor.total_donations).to eq(200) }
   end
+
+  describe ".search" do
+    let(:match_beginning) { create(:donor, name: "Smithie Black", identification: "G1234567M") }
+    let(:match_middle) { create(:donor, name: "Bob Smith-White", identification: "G1234567M") }
+    let(:match_end) { create(:donor, name: "John Smith", identification: "G1234567M") }
+    let(:no_match) { create(:donor, name: "Carl Grey", identification: "G1234567M") }
+
+    it { expect(described_class.search("smith")).to contain_exactly(match_beginning, match_middle, match_end) }
+  end
 end
