cf3e76f58dd3c6fd3872f101b4e954619c0ad923
alahab <maria.dku.kz@gmail.com>
2016-11-02 17:33:28 +0800

    [Fix #70] Make "Dashboard" homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "donations#index"
   resources :donations, only: [:create, :index, :update, :edit]
   resources :donors, only: [:index, :show, :update, :edit]
   resources :events, only: [:create, :index, :show, :update, :edit]
