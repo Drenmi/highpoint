ad27021a6beddf3975998f0dd9fed032e02dac49
alahab <maria.dku.kz@gmail.com>
2016-11-02 18:10:17 +0800

    Add users/show and update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :donations, only: [:create, :index, :update, :edit]
   resources :donors, only: [:index, :show, :update, :edit]
   resources :events, only: [:create, :index, :show, :update, :edit]
+  resources :users, only: [:show]
 end
