980dca77206d3f54805291eec281449d552ca1d4
limxinyuuu <limxinyuuu@gmail.com>
2016-11-05 20:38:53 +0800

    Edit column filters in events & donors index views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def sortable(column)
+    css_class =
+      if column.to_s == params[:sort]
+        if params[:direction] == "ASC"
+          "fa fa-caret-up fa-lg text-success"
+        else
+          "fa fa-caret-down fa-lg text-success"
+        end
+      else
+        "fa fa-sort fa-lg"
+      end
+    direction =
+      if column.to_s == params[:sort] && params[:direction] == "ASC"
+        "DESC"
+      else
+        "ASC"
+      end
+    link_to "", safe_params(sort: column.to_s, direction: direction), class: css_class
+  end
 end
