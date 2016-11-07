3141660ce67f6ad6659bce3bd48470926f48a8c0
alahab <maria.dku.kz@gmail.com>
2016-11-07 15:37:38 +0800

    Edit views Raise an exception if there is an error when sending an email Validate input

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -7,7 +7,7 @@ Clearance.configure do |config|
   config.cookie_path = "/"
   config.routes = true
   config.httponly = false
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = "test@hcsa.org.sg"
   config.password_strategy = Clearance::PasswordStrategies::BCrypt
   config.redirect_url = "/"
   config.secure_cookie = false
