192b82ddcb9f7ba1938d65548e426254cd34c6bc
alahab <maria.dku.kz@gmail.com>
2016-11-23 22:04:56 +0800

    [Fix #121] Add strong params to reports - Report doesn't throw an error if a month is chosen without a year

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,13 @@
 class ReportsController < ApplicationController
   def create
-    @report = Report.new(params)
+    @report = Report.new(donation_params)
     render :index
   end
 
   def index
+  end
+
+  def donation_params
+    params.permit({ date: [:year, :month] }, :cause_id, :event_id)
   end
 end
